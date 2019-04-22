6f974711db438e61c7d95e3ba709109d02abd3dc
ihateclownz <jisatsu_suru@msn.com>
2019-04-21 21:58:20 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        public static Dictionary<string, string> Spanish2Eng = new Dictionary<string, string>();
+        static void Main()
+        {
+            //Iterate over the list 
+            foreach (var item in WordList.Eng2Spanish)
+            {
+                Spanish2Eng.Add(item.Value, item.Key);
+            }
+
+            Intro();
+        }
+        public static void Intro()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("1: English");
+            Console.WriteLine("2: Spanish");
+
+            Translate.Trans(Console.ReadLine());
+        }
+        public static class Translate
+        {
+            public static void Trans(string selection)
+            {
+                string input = "", lang1 = "", lang2 = "";
+                
+                if (selection == "1")
+                {
+                    lang1 = "English: ";
+                    lang2 = "Spanish: ";
+                }
+                else if (selection == "2")
+                {
+                    lang1 = "Spanish: ";
+                    lang2 = "English: ";
+                }
+
+                Console.Write("{0}: ",lang1);
+                // Console.Write(lang1 + ": ");  This is another way of writing the same line. Called String Concatenation.
+                input = Console.ReadLine().ToLower();
+
+                if (WordList.Eng2Spanish.ContainsKey(input))  // Looks through the dictionary to see if the word is english and has a match
+                {
+                    Console.WriteLine(lang2 + WordList.Eng2Spanish[input]);
+                }
+                else if (Spanish2Eng.ContainsKey(input)) // Looks through the dictionary to see if the word is spanish and has a match
+                {
+                    Console.WriteLine(lang2 + Spanish2Eng[input]);
+                }
+                else  // neither the word the user inputed is in the English or Spanish dictionary 
+                {
+                    Console.WriteLine("Word does not exist, please try again.");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+                Intro();
+            }
+        } 
+    }
+}
---
+++ ConsoleApp3/WordList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    public static class WordList
+    {
+        public static Dictionary<string, string> Eng2Spanish = new Dictionary<string, string>
+        {
+            {"hello", "hola" },
+            {"goodbye","adios" },
+            {"good afternoon", "buenos tardes" },
+            {"good night", "buenos noches" },
+            {"bad", "malo" },
+            {"water", "agua" },
+            {"friends", "amigos" },
+            {"poor", "pobre"},
+            {"escape", "escapar" },
+            {"dream", "sueno"},
+            {"sleep", "dormir"},
+            {"cool","guay" },
+            {"world", "mundo" },
+            {"easy", "fácil" },
+            {"mirror", "espejo" },
+            {"glass", "vaso" },
+            {"wall", "pared" },
+            {"old","antiguo" },
+            {"town", "pueblo" },
+            {"road", "la carretera" },
+            {"pretty", "bonita" },
+            {"grow", "crecer" },
+            {"spoil", "mimar" },
+            {"never", "nunca" },
+            {"evil","mal" },
+            {"dog", "perro" },
+            {"cat", "gato" },
+            {"book", "libro"},
+            {"music", "música"},
+            {"animals", "animales"},
+            {"attack", "ataque"},
+            {"chess", "ajedrez" },
+            {"movie", "película" },
+            {"pictures", "imágenes" },
+            {"memory", "memoria" },
+            {"touch", "toque" },
+            {"hate", "odio" },
+            {"forget", "olvidar"},
+            {"happy", "feliz" },
+            {"party", "partido" },
+            {"tears", "lagrimas" },
+            {"apart","aparte" },
+            {"mood", "estado animico" },
+            {"promise", "promesa" },
+            {"cold", "frio" },
+            {"break", "descanso" },
+            {"love", "amor" },
+            {"crime", "crimen" },
+            {"watch", "reloj" }
+        };
+    }
+}
